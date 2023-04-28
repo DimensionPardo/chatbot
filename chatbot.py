dffd7e89b44f779f553f1afbe94c19353f430389
Juan Pablo Pardo Pérez <39962281+DimensionPardo@users.noreply.github.com>
2023-04-28 10:28:49 +0200

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -9,44 +9,52 @@ from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
 
+#Importamos los archivos generados en el código anterior
+intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.h5')
 
+#Pasamos las palabras de oración a su forma raíz
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
-def bag_of_words(sentence): #Hace una lista con unos y ceros según la palabra que encontró en el mensaje
+#Convertimos la información a unos y ceros según si están presentes en los patrones
+def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0]*len(words)
     for w in sentence_words:
         for i, word in enumerate(words):
             if word == w:
-                bag[i] = 1
+                bag[i]=1
+    print(bag)
     return np.array(bag)
 
-def predict_class(sentence): #Hace las predicciones con las palabras que tenemos y nos dice a qué clase pertenecen
+#Predecimos la categoría a la que pertenece la oración
+def predict_class(sentence):
     bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0] #Probabilidad de que sea cada una de las categorías
-    max_index = np.where(res==np.max(res))[0][0] #Indice de la que tiene mayor probabilidad
-    category = classes[max_index] #Categoría con ese índice
+    res = model.predict(np.array([bow]))[0]
+    max_index = np.where(res ==np.max(res))[0][0]
+    category = classes[max_index]
     return category
 
+#Obtenemos una respuesta aleatoria
 def get_response(tag, intents_json):
     list_of_intents = intents_json['intents']
     result = ""
     for i in list_of_intents:
-        if i['tag']==tag:
+        if i["tag"]==tag:
             result = random.choice(i['responses'])
             break
     return result
 
+#Ejecutamos el chat en bucle
 while True:
-    message = input("")
+    message=input("")
     ints = predict_class(message)
     res = get_response(ints, intents)
     print(res)
+    
--- training.py
+++ training.py
@@ -4,9 +4,9 @@ import pickle
 import numpy as np
 
 import nltk
-from nltk.stem import WordNetLemmatizer
-import talos
+from nltk.stem import WordNetLemmatizer #Para pasar las palabras a su forma raíz
 
+#Para crear la red neuronal
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import sgd_experimental
@@ -19,54 +19,58 @@ nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
-
 words = []
 classes = []
 documents = []
-ignore_letters = ['?', '!', '.', ',']
+ignore_letters = ['?', '!', '¿', '.', ',']
 
-
+#Clasifica los patrones y las categorías
 for intent in intents['intents']:
-    for pattern in intent["patterns"]:
+    for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
         documents.append((word_list, intent["tag"]))
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
-print(classes)
 
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
-
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
+
+#Pasa la información a unos y ceros según las palabras presentes en cada categoría para hacer el entrenamiento
 training = []
-output_empty = [0] * len(classes)
+output_empty = [0]*len(classes)
 for document in documents:
     bag = []
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
-    output_row =list(output_empty)
+    output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training) 
+print(training) 
 
-
+#Reparte los datos para pasarlos a la red
 train_x = list(training[:,0])
-train_y = list(training[:, 1])
+train_y = list(training[:,1])
 
+#Creamos la red neuronal
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation="softmax"))
+model.add(Dense(len(train_y[0]), activation='softmax'))
 
+#Creamos el optimizador y lo compilamos
 sgd = sgd_experimental.SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save("chatbot_model.h5", hist)
+model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+#Entrenamos el modelo y lo guardamos
+train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+model.save("chatbot_model.h5", train_process)
